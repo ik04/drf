fef9088750394a605cf5cce4640ab98bf526a288
ik04 <ishaankhurana123@gmail.com>
2024-02-11 06:13:28 +0530

    feat: make media_root dynanmic

--- prac/prac/settings.py
+++ prac/prac/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,8 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'storage/'
-MEDIA_ROOT = '/home/ishaan/Documents/dev/Practice/django/drf/prac/storage'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
